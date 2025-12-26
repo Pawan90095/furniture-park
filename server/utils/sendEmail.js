5f85d6124803d9316d65c72d7f0d59466e81e772
Pawan90095 <pavanrewar027@gmail.com>
2025-12-26 13:45:57 +0530

    Add files via upload

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,32 +1,33 @@
 import nodemailer from 'nodemailer';
 
-console.log(`Sending email to: ${options.email} with host: smtp.gmail.com`);
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    connectionTimeout: 10000, // 10 seconds
-    greetingTimeout: 5000, // 5 seconds
-    auth: {
-        user: process.env.SMTP_EMAIL,
-        pass: process.env.SMTP_PASSWORD.replace(/\s+/g, ''),
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-});
+const sendEmail = async (options) => {
+    console.log(`Sending email to: ${options.email} with host: smtp.gmail.com`);
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        connectionTimeout: 10000, // 10 seconds
+        greetingTimeout: 5000, // 5 seconds
+        auth: {
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD.replace(/\s+/g, ''),
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
-const message = {
-    from: `${process.env.FROM_NAME} <${process.env.SMTP_EMAIL}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: options.html // Can add HTML templates later
-};
+    const message = {
+        from: `${process.env.FROM_NAME} <${process.env.SMTP_EMAIL}>`,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+        // html: options.html // Can add HTML templates later
+    };
 
-const info = await transporter.sendMail(message);
+    const info = await transporter.sendMail(message);
 
-console.log('Message sent: %s', info.messageId);
+    console.log('Message sent: %s', info.messageId);
 };
 
 export default sendEmail;
