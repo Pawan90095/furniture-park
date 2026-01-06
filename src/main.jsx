a26d520e01e04129a4d5ff207b6a09f8194e495a
Furniture Park Owner <orders.furniturepark@gmail.com>
2026-01-06 14:43:36 +0530

    fix: add error boundary and restore import

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import GlobalErrorBoundary from './components/GlobalErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId="950922748030-tpnqpnmo9pk7rt930g7br7mp4glkaa52.apps.googleusercontent.com">
-            <BrowserRouter>
-                <CartProvider>
-                    <App />
-                </CartProvider>
-            </BrowserRouter>
-        </GoogleOAuthProvider>
+        <GlobalErrorBoundary>
+            <GoogleOAuthProvider clientId="950922748030-tpnqpnmo9pk7rt930g7br7mp4glkaa52.apps.googleusercontent.com">
+                <BrowserRouter>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </BrowserRouter>
+            </GoogleOAuthProvider>
+        </GlobalErrorBoundary>
     </React.StrictMode>,
 )
