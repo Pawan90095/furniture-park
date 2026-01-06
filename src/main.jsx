ce03ff22d4ba5def53f0885be85c1cecfc3fe42c
Furniture Park Owner <orders.furniturepark@gmail.com>
2026-01-06 14:46:53 +0530

    fix: restore CartProvider import

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import GlobalErrorBoundary from './components/GlobalErrorBoundary';
+import { CartProvider } from './context/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
