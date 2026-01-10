f93163f3e75d87e0aa6b633784b4a7ded3bd165b
Furniture Park Owner <orders.furniturepark@gmail.com>
2026-01-10 18:06:20 +0530

    fix: use provided cloudinary keys as fallbacks

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -3,10 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'djusefan5',
+    api_key: process.env.CLOUDINARY_API_KEY || '227448376867724',
+        api_secret: process.env.CLOUDINARY_API_SECRET || 'w-2v7Yt_uGfC6vX_L7gK4s-1Y-0',
 });
 
 export default cloudinary;
